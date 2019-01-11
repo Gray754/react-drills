af1153e1ec80ded34b1e238e3478fb8513def8b8
Gray754 <lgc9312@gmail.com>
2019-01-11 16:41:49 -0600

    finished app-5

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Image from './Image'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Image memeMachine={"https://i.pinimg.com/originals/13/88/a2/1388a2ca15eea0af64c82efeb3ed3344.jpg"}/>
       </div>
     );
   }
