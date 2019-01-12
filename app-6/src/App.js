293481cd936b4a2488f8329b33a672b986fcbddc
Gray754 <lgc9312@gmail.com>
2019-01-11 19:40:17 -0600

    finished app-6

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,18 +1,43 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Todo from './Todo'
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state={
+      todoList:[],
+      inputStr:''
+    }
+  }
+
+  handleChange=(e)=>{
+    this.setState({inputStr:e})
+  }
+
+  handleClick=()=>{
+    var listOfStuff = this.state.todoList.map((e)=>e)
+    listOfStuff.push(this.state.inputStr);
+
+    this.setState({todoList:listOfStuff});
+  }
+
+
   render() {
+    let sortList=this.state.todoList.map((e,i)=>{
+        return (
+          <Todo key={i} todo={e}/>
+          // <h3 key={i}>{e}</h3>
+        )
+    })
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>My to-do list:</h2>
+        <input onChange={(e)=>this.handleChange(e.target.value)} placeholder="Enter new task" />
+        <button onClick={this.handleClick}>Click Me</button>
+        {sortList}
       </div>
     );
   }
